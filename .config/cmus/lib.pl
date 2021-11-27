6cef83a3e3574e5b40721bbd97e2ace6d410da89
mxhectorvega <mxhectorvega@gmail.com>
2021-11-26 18:06:28 -0600

    act reg vie 26 nov 21 18:06

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,72 @@
+/home/vega/Música/Lofi/Call me - 90s Flav.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Children Of The World - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Fanny (Be Tender With My Love) - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/For Whom The Bell Tolls - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Holiday - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/How Can You Mend A Broken Heart - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/How Deep Is Your Love - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/I Started A Joke - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/I ve Gotta Get A Message To You - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Jive Talkin  - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Love So Right - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Love You Inside Out - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Massachussetts - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/More Than A Woman - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/New York Mining Disaster 1941 - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Night Fever - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Nights On Broadway - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Run To Me - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Spicks And Specks - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Stayin Alive - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/To Love Somebody - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Too Much Heaven - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Tragedy - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Wind Of Change - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/Words - Bee Gees.mp3
+/home/vega/Música/Bee Gees - How Can You Mend A Broken Heart/You Should Be Dancing - Bee Gees.mp3
+/home/vega/Música/Lofi/Peach Tree - Biosphere.mp3
+/home/vega/Música/Lofi/Sorry, I Like You - Burbank.mp3
+/home/vega/Música/Lofi/1. Clairo - Flaming Hot Cheetos (128).mp3
+/home/vega/Música/Lofi/Spongebob (Instrumental) - Common Cents.mp3
+/home/vega/Música/Lofi/I Just Want to Be the One You Love - Cryst.mp3
+/home/vega/Música/Lofi/Redbone - Dontcry.mp3
+/home/vega/Música/Lofi/Her - eery.mp3
+/home/vega/Música/Lofi/this girl - Elijah Who.mp3
+/home/vega/Música/Lofi/Resonance - Home.mp3
+/home/vega/Música/Lofi/controlla - Idealism.mp3
+/home/vega/Música/Lofi/Take Me to Pluto - idonthateyou.mp3
+/home/vega/Música/Lofi/To the Girl With Red Converse - Jackie Xan.mp3
+/home/vega/Música/Lofi/Affection - Jinsang.mp3
+/home/vega/Música/Lofi/Tired Boy - Joey Pecoraro.mp3
+/home/vega/Música/Lofi/worldstar money (interlude) - Joji.mp3
+/home/vega/Música/Lofi/i m in love with u, sorry - J San.mp3
+/home/vega/Música/Lofi/Her - Kendall Miles.mp3
+/home/vega/Música/Lofi/Aesthetic - Kingmichaelbeats.mp3
+/home/vega/Música/Lofi/Gravity Falls - L.Dre.mp3
+/home/vega/Música/Lofi/Steven Universe - L.Dre.mp3
+/home/vega/Música/Lofi/Tokyo (Lo-Fi version) - Leat eq.mp3
+/home/vega/Música/Lofi/fountain - Leavv.mp3
+/home/vega/Música/Lofi/Day in Paris - Llusion.mp3
+/home/vega/Música/Lofi/Uh Oh Stinky but It s a Soft Lofi Beat (feat. Egg) - Llusion.mp3
+/home/vega/Música/Lofi/Falling for U - Peachy!.mp3
+/home/vega/Música/Lofi/This Is What Happens - Peachy!.mp3
+/home/vega/Música/Lofi/Wine and Roses - Peachy!.mp3
+/home/vega/Música/Lofi/i m closing my eyes (feat. shiloh) - potsu.mp3
+/home/vega/Música/Lofi/lovesick - potsu.mp3
+/home/vega/Música/Lofi/death bed (coffee for your head) - Powfu.mp3
+/home/vega/Música/Lofi/the way that you see me (feat. Ayleen Valentine) - Powfu.mp3
+/home/vega/Música/Lofi/Getsomerestsleepwell - Quickly Quickly.mp3
+/home/vega/Música/Lofi/Sunflower - Rex Orange County.mp3
+/home/vega/Música/Lofi/Sakura Trees - Saib.mp3
+/home/vega/Música/Lofi/change ur mind - Sarcastic Sounds.mp3
+/home/vega/Música/Lofi/I ll Bite Your Soul - SHRK.mp3
+/home/vega/Música/Lofi/Eyes Blue Like The Atlantic (feat. Subvrbs) - Sista Prod.mp3
+/home/vega/Música/Lofi/I m Sorry - Swell.mp3
+/home/vega/Música/Lofi/スモーク - SwuM.mp3
+/home/vega/Música/Lofi/who put blood in my drink  - SYML.mp3
+/home/vega/Música/Lofi/5 32pm - The Deli.mp3
+/home/vega/Música/Lofi/Monday Loop - Tomppabeats.mp3
+/home/vega/Música/Lofi/Who  (feat. Shiloh Dynasty) - TSUKI.mp3
+/home/vega/Música/Lofi/5. Soundgarden - Black Hole Sun (128).mp3
+/home/vega/Música/Lofi/The Loser - Verzache.mp3
+/home/vega/Música/Lofi/Aesthetic Girl - Yusei.mp3
